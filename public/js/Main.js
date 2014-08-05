a13628dcf33ea1ee6502ad084491e3834ac29fe7
Amin Meyghani <amin.meyghani@quovo.com>
2014-08-05 08:29:05 -0400

    add basic angular routes + express routes

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,1 +1,7 @@
-angular.module('MainApp', ['ui.bootstrap','ModuleOne'])
+angular.module('MainApp', ['ui.bootstrap','ModuleOne', 'ngRoute'])
+.config(['$routeProvider', function($routeProvider) { $routeProvider
+	.when('/', { templateUrl:"template.htm" })
+	.when('/404', { templateUrl:"404.htm" })
+	.when('/dashboard', { templateUrl:"randomfile.htm" })
+	.otherwise({redirectTo:'/404'});
+}])
--- server/serverSettings.js
+++ server/serverSettings.js
@@ -3,8 +3,10 @@ var express  = require('express');
 var path     = require('path');
 var app      = express();
 var serveFolder = "public";
+var middleware = require('middleware');
+var indexFile = "/index.htm";
 var _settings = {
-	port: 8123,
+	port: 3000,
 	rootPath: "/",
 	servePath: "/../"+serveFolder
 };
@@ -15,8 +17,37 @@ app.use(express.static(__dirname + _settings.servePath));
 
 // when the request comes in for "/", get index with respect to the current directory of the app.
 app.get( _settings.rootPath, function( req, res ){
-    res.sendfile(serveFolder+'/index.htm', { root: "."});
+	console.log("sending index over ...")
+	// res.status(400).end()
+    res.sendfile(serveFolder+indexFile, { root: "."});
 });
+
+app.get('*', function(req, res, next) {
+	console.log("there was here alll catch ***************")
+	res.status(404);
+	// res.sendfile(serveFolder+"/index.htm", { root: "."});
+	// res.sendfile(serveFolder+'/index.htm', { root: "."});
+ //  var err = new Error();
+ //  console.log(err.status);
+ //  err.status = 404;
+ //  next(err);
+ // res.status(400).end()
+});
+ 
+// handling 404 errors
+// app.use(function(err, req, res, next) {
+//   if(err.status !== 404) {
+//     return next();
+//   }
+//   res.send(err.message || 'nothing found ....');
+// });
+
+/*
+app.get('/partials/:partial', function(req, res) {
+    return res.render('partials/' + req.params['partial']);
+};
+*/
+
 
 // am: expose api to be used by the www script runner.
 var exp = {  
