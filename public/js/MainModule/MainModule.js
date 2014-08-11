43fc37b7ddfca9b45ee6c3ad73108c023dfa4d14
Amin Meyghani <amin.meyghani@quovo.com>
2014-08-10 22:11:27 -0400

    add scroller

--- public/js/MainModule/MainModule.js
+++ public/js/MainModule/MainModule.js
@@ -225,3 +225,30 @@ angular.module('MainModule', ['MainApp'])
   });  
 }})
 
+// full height
+.directive('fullheight', [ "$window", function($window) {
+ return function(scope, element, attrs) {
+  var windowH = $(window).height();
+  var wrapperH = $(element).height();
+  if(windowH > wrapperH) {                            
+    $(element).css({'height':($(window).height())+'px'});
+  }                                                                               
+  $(window).resize(function(){
+    var windowH = $(window).height();
+    var wrapperH = $(element).height();
+    var differenceH = windowH - wrapperH;
+    var newH = wrapperH + differenceH;
+    var truecontentH = $('#truecontent').height();
+    if(windowH > truecontentH) {
+      $(element).css('height', (newH)+'px');
+    }
+
+  });
+
+}
+}])
+
+// custom scrollbar
+.directive('scroller', function() { return function(scope, element, attrs) {
+  $(element).perfectScrollbar({wheelSpeed : 20});
+}})
